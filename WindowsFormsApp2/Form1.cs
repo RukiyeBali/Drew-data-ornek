0247cc04a13270b37c6d6ae71acefa2e6b00d073
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-12-05 14:07:05 +0300

    Add files via upload

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Ogrenci ogrenci;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<Ogrenci> ogrenciListe = new List<Ogrenci>();
+
+            ogrenciListe.Add(new Ogrenci(1533, "Rukiye Balı","10/A", "05156328471"));
+            ogrenciListe.Add(new Ogrenci(1544, "İlayda Daban", "10/A", "05387593647"));
+            ogrenciListe.Add(new Ogrenci(1545, "Fatih Yiner", "10/A", "05346982145"));
+            ogrenciListe.Add(new Ogrenci(1515, "Behiye Öztürk", "10/A", "05684256892"));
+            ogrenciListe.Add(new Ogrenci(1514, "Toprak Akın", "10/A", "0532859395"));
+            ogrenciListe.Add(new Ogrenci(1533, "Mahmet Arıkan", "10/A", "05347723416"));
+            ogrenciListe.Add(new Ogrenci(1533, "Ahmet Cansever", "16/A", "05322684195"));
+
+            dataGridView1.DataSource = ogrenciListe.ToList();
+
+
+
+
+
+        }
+    }
+}
---
+++ WindowsFormsApp2/Ogrenci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    internal class Ogrenci
+    {
+        int numara;
+        string adSoyad;
+        string sinif;
+        string telefon;
+
+        public int Numara { get => numara; set => numara = value; }
+        public string AdSoyad { get => adSoyad; set => adSoyad = value; }
+        public string Sinif { get => sinif; set => sinif = value; }
+        public string Telefon { get => telefon; set => telefon = value; }
+
+        public Ogrenci(int numara, string adSoyad, string sinif, string telefon)
+        {
+            this.numara = numara;
+            this.adSoyad = adSoyad;
+            this.sinif = sinif;
+            this.telefon = telefon;
+        }
+    }
+}
